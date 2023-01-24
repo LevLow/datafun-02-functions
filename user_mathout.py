d338de6a0686e0efbc70a30cac7148181963f0f5
LevLow <levilowther@gmail.com>
2023-01-24 00:51:11 -0600

    First sync test

---
+++ user_mathout.py
@@ -0,0 +1,22 @@
+(base) PS C:\Users\levil\Documents\datafun-02-functions> & C:/Users/levil/AppData/Local/Programs/Python/Python310/python.exe c:/Users/levil/Documents/datafun-02-functions/user_math.py
+Explore some functions in the math module
+
+math.comb(5,1) = 5
+math.perm(5,1) = 5
+
+math.comb(5,3) = 10
+math.perm(5,3) = 60
+
+Call get_area_of_lot with different arguments
+
+get_area_of_lot(6,2) = 12
+get_area_of_lot(10,-2) = -20
+Error: can't multiply sequence by non-int of type 'str'
+get_area_of_lot(6,2) = None
+
+donation_round_up(25.30) = 26
+
+art_class_cost(3) = 180
+
+sum_of_fundraiser(2) = 4400.0
+(base) PS C:\Users\levil\Documents\datafun-02-functions>
