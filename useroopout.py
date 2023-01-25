3b03881fee930a4f4e6e34f287d315cbf9449405
LevLow <levilowther@gmail.com>
2023-01-24 23:09:46 -0600

    Final Push

---
+++ useroopout.py
@@ -0,0 +1,77 @@
+(base) PS C:\Users\levil\Documents\datafun-02-functions> & C:/Users/levil/AppData/Local/Programs/Python/Python310/python.exe c:/Users/levil/Documents/datafun-02-functions/useroop.py
+
+Welcome, I'm a new PyBuddy.
+
+I'm Alice.
+I'm a Species.CAT with 4 legs and 2 ear(s).
+I weigh 8.12 kgs.
+I've been alive for 0:00:00.001001.
+I've created over 0 works of art.I'm available for tutoring.
+I know:
+  - Git
+  - GitHub
+  - Python
+  - Markdown
+  - VS Code
+If you double the number of art works I have created you get 0.
+My previous donations include, 5, 10, 7, 15, 25, 50,  and the average of my dontaions is 18.67
+
+
+        You'll need curiousity, the ability to search the web,
+        and the tenacity and resourcefulness
+        to solve all kinds of challenges.
+
+        Let's get started!
+
+
+
+Welcome, I'm a new PyBuddy.
+
+I'm Rex.
+I'm a Species.DOG with 4 legs and 2 ear(s).
+I weigh 10.44 kgs.
+I've been alive for 0:00:00.001001.
+I've created over 0 works of art.I'm available for tutoring.
+I know:
+  - Git
+  - GitHub
+  - Python
+  - Markdown
+  - VS Code
+If you double the number of art works I have created you get 0.
+My previous donations include, 0, 2, 3,  and the average of my dontaions is 1.67
+
+
+        You'll need curiousity, the ability to search the web,
+        and the tenacity and resourcefulness
+        to solve all kinds of challenges.
+
+        Let's get started!
+
+
+
+Welcome, I'm a new PyBuddy.
+
+I'm Vincent.
+I'm a Species.ARTIST with 2 legs and 1 ear(s).
+I weigh 75.24 kgs.
+I've been alive for 0:00:00.
+I've created over 900 works of art.I'm already helping others learn Python.
+I know:
+  - Painting
+  - Warm
+  - Cool
+  - Self Portraits
+  - Being Dramatic
+If you double the number of art works I have created you get 1800.
+My previous donations include, 2, 3, 9, 7, 1, 4, 5,  and the average of my dontaions is 4.43
+
+
+        You'll need curiousity, the ability to search the web,
+        and the tenacity and resourcefulness
+        to solve all kinds of challenges.
+
+        Let's get started!
+
+
+(base) PS C:\Users\levil\Documents\datafun-02-functions>
